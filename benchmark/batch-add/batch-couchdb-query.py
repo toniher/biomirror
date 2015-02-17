62ef3a505424adc4c820e7bda69b01392d16575a
toniher <toniher@cau.cat>
2015-02-17 17:51:00 +0100

    query

---
+++ benchmark/batch-add/batch-couchdb-query.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import hashlib
+import couchdb
+
+def main(argv):
+
+		# Put stuff in JSON config file
+		couchServer = 'http://localhost:5984/'
+		couchDB = 'testseq'
+		
+		couch = couchdb.Server(couchServer)
+
+		db = couch[couchDB]
+
+		seqDoc = db.get( argv[0] )
+		print seqDoc['seq']
+
+if __name__ == "__main__":
+		main(sys.argv[1:])
