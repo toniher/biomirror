227be5ef32bf5f2fc5a658d15662f76e1d98de17
Toni Hermoso Pulido <toniher@cau.cat>
2015-02-18 00:13:24 +0100

    adding redis

---
+++ benchmark/batch-add/batch-redis-add.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import redis
+from Bio import SeqIO
+
+
+def main(argv):
+
+		# Put stuff in JSON config file
+
+		r=redis.Redis()  
+
+		batch = 1000;
+		itera = 0
+
+		pipeline=r.pipeline()
+		handle = open( argv[0], "r")
+		for record in SeqIO.parse(handle, "fasta") :
+				pipeline.set( str( record.id ), str( record.seq ) )
+				itera = itera + 1
+				if itera > batch :
+					pipeline.execute()
+					itera = 0
+		
+		if itera > 0 :
+				pipeline.execute()
+
+		handle.close()
+
+
+if __name__ == "__main__":
+		main(sys.argv[1:])
