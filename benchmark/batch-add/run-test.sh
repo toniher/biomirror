8a373b909483e9ca2fc1cede3278c5097c873b36
Toni Hermoso Pulido <toniher@cau.cat>
2015-02-18 01:18:15 +0100

    run test

---
+++ benchmark/batch-add/run-test.sh
@@ -0,0 +1,28 @@
+#/bin/sh
+
+echo "BLAST"
+time ./batch-ncbiblast-add.sh ../datasets/drosoph.aa.md5
+time ./batch-ncbiblast-query.sh
+
+echo "SAMTOOLS"
+time ./batch-samtools-add.sh ../datasets/drosoph.aa.md5
+time ./batch-samtools-query.sh
+
+echo "COUCHDB"
+
+time python batch-couchdb-add.py ../datasets/drosoph.aa.md5
+time python batch-couchdb-query.py 7d3bb4ae52311b22e8e7daf4daf4587e
+
+echo "SQLITE"
+time python batch-sqlite-add.py ../datasets/drosoph.aa.md5
+time python batch-sqlite-query.py 7d3bb4ae52311b22e8e7daf4daf4587e
+
+echo "REDIS"
+time python batch-redis-add.py ../datasets/drosoph.aa.md5
+time python batch-redis-query.py 7d3bb4ae52311b22e8e7daf4daf4587e
+
+
+
+
+
+
