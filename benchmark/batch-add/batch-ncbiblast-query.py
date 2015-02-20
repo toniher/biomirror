ef69a33725d459ec22ee9f050f77acf4e9cd7992
toniher <toniher@cau.cat>
2015-02-20 09:19:49 +0100

    fix

--- benchmark/batch-add/batch-ncbiblast-query.py
+++ benchmark/batch-add/batch-ncbiblast-query.py
@@ -3,7 +3,7 @@ import subprocess
 
 def main(argv):
 
-		p = subprocess.Popen("blastdbcmd -dbtype prot -entry "+argv[1]+" -db "+argv[0]+", stdout=subprocess.PIPE, shell=True)
+		p = subprocess.Popen("blastdbcmd -dbtype prot -entry "+argv[1]+" -db "+argv[0], stdout=subprocess.PIPE, shell=True)
 		(output, err) = p.communicate()
 
 		print output
