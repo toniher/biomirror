e2c57a52120be0a5b889ea5051268c11e2de29da
Toni Hermoso Pulido <toniher@cau.cat>
2015-02-22 01:07:20 +0100

    add split

---
+++ benchmark/batch-add-dir/splitindir.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+FASTA=$1
+EXTENT=100000
+DIRNAME=`dirname $FASTA`
+DIREND="../dir"
+
+echo $DIRNAME
+
+NUMSEQ=`cat $1|grep '>'|wc -l`
+
+NUMFILES=$(echo "a=$NUMSEQ; b=$EXTENT; if ( a%b ) a/b+1 else a/b" | bc)
+
+echo $NUMFILES
+pyfasta split -n $NUMFILES $FASTA
+
+mkdir -p $DIREND
+export DIREND
+export DIRNAME
+
+ls -lR $DIRNAME | perl -lane 'if ($_=~/(\S+\.\d+|\S+\.split)/ ) { system("mv $ENV{DIRNAME}/$1 $ENV{DIREND}");  }'
+
+
+
