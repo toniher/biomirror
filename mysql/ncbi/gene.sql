5ee821a2fceb484a0de0da26ff286e601df43d1e
toniher <toniher@cau.cat>
2015-11-25 11:08:35 +0100

    Redoing sql and tables

--- mysql/ncbi/gene.sql
+++ mysql/ncbi/gene.sql
@@ -5,16 +5,10 @@ CREATE TABLE `gene_info` (
   `Symbol` varchar(16) NOT NULL default '',
   `LocusTag` varchar(16) NOT NULL default '',
   `Synonyms` varchar(16) NOT NULL default '',
-  `dbXrefs` varchar(16) NOT NULL default '',
   `chromosome` varchar(16) NOT NULL default '',
   `map_location` varchar(16) NOT NULL default '',
   `description` text NOT NULL default '',
-  `type_of_gene` varchar(16) NOT NULL default '',
-  `Symbol_from_nomenclature_authority` varchar(16) NOT NULL default '',
-  `Full_name_from_nomenclature_authority` varchar(16) NOT NULL default '',
-  `Nomenclature_status` varchar(16) NOT NULL default '',
-  `Other_designations` varchar(16) NOT NULL default '',
-  `Modification_date` varchar(16) NOT NULL default '',
+  `type_of_gene` varchar(16) NOT NULL default ''
   KEY `index_geneid` (`GeneID`),
   KEY `index_symbol` (`Symbol`),
   KEY `index_tax_id` (`tax_id`)
@@ -22,25 +16,47 @@ CREATE TABLE `gene_info` (
 
 drop table if exists `gene2accession`;
 CREATE TABLE `gene2accession` (
-`tax_id` int(11) NOT NULL default '0',
 `GeneID` int(11) NOT NULL default '0',
-`status` varchar(16) NOT NULL default '',
-`RNA_nucleotide_accession` varchar(16) NOT NULL default '',
 `RNA_nucleotide_gi` varchar(16) NOT NULL default '',
-`protein_accession` varchar(16) NOT NULL default '',
 `protein_gi` varchar(16) NOT NULL default '',
-`genomic_nucleotide_accession` varchar(255) NOT NULL default '',
 `genomic_nucleotide_gi` varchar(16) NOT NULL default '', 
 `start_position` varchar(16) NOT NULL default '',
 `end_positon` varchar(16) NOT NULL default '',
 `orientation` varchar(16) NOT NULL default '',
 `assembly` varchar(16) NOT NULL default '',
 KEY `index_geneid` (`GeneID`),
-KEY `index_accession` (`protein_accession`),
 KEY `index_protein_gi` (`protein_gi`),
 KEY `index_genomic_nucleotide_gi` (`genomic_nucleotide_gi`),
-KEY `index_RNA_nucleotide_gi` (`RNA_nucleotide_gi`),
-KEY `index_tax_id` (`tax_id`)
+KEY `index_RNA_nucleotide_gi` (`RNA_nucleotide_gi`)
 ) ENGINE=MyISAM;
+
+drop table if exists `gene_group`;
+CREATE TABLE `gene_group` (
+  `GeneID` int(11) NOT NULL default '0',,
+  `relationship` varchar(50) NOT NULL default '',
+  `GeneID_match` int(11) NOT NULL default '0',
+  KEY `index_geneid` (`GeneID`),
+  KEY `index_relationship` (`relationship`),
+  KEY `index_geneid_match` (`GeneID_match`)
+) ENGINE=MyISAM;
+
+drop table if exists `gene2pubmed`;
+CREATE TABLE `gene2pubmed` (
+  `GeneID` int(11) NOT NULL default '0',,
+  `PubmedID` int(11) NOT NULL default '0',
+  KEY `index_geneid` (`GeneID`),
+  KEY `index_pubmedid` (`PubmedID`)
+) ENGINE=MyISAM;
+
+drop table if exists `gene2go`;
+CREATE TABLE `gene2go` (
+  `GeneID` int(11) NOT NULL default '0',,
+  `GO` varchar(20) NOT NULL default '',
+  `evidence` varchar(8) NOT NULL default '',
+  KEY `index_geneid` (`GeneID`),
+  KEY `index_go` (`GO`),
+  KEY `index_evidence` (`evidence`)
+) ENGINE=MyISAM;
+
 
 
