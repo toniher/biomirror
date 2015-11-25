2dbf209b483a96a15262efaeb0906f3203f2b422
toniher <toniher@cau.cat>
2015-11-25 14:08:47 +0100

    escape

--- mysql/ncbi/gene_info.py
+++ mysql/ncbi/gene_info.py
@@ -69,7 +69,7 @@ def main(argv):
                         if ( row[0].startswith('#') ): #Avoid row with !
                                 continue
 
-                        cursor.execute('INSERT INTO gene_info VALUES("'+row[0]+'", "'+row[1]+'", "'+row[2]+'", "'+row[3]+'", "'+row[4]+'", "'+row[6]+'", "'+row[7]+', "'+row[8]+'")')
+                        cursor.execute('INSERT INTO gene_info VALUES("'+row[0]+'", "'+row[1]+'", "'+row[2]+'", "'+row[3]+'", "'+row[4]+'", "'+row[6]+'", "'+row[7].replace("'", "\\'")+', "'+row[8]+'")')
                         i = i+1
                         if (i == limit):
                                 i=0
