942a9a6847682f7bcce224f712a65278631b818f
toniher <toniher@cau.cat>
2016-04-13 14:46:53 +0200

    some modifs

--- neo4j/synonyms-dir.sh
+++ neo4j/synonyms-dir.sh
@@ -1,8 +1,12 @@
 #DIR of parts
 DIR=$1
 
+echo "CREATE INDEX ON :MOL(synonyms);" > /data/toniher/script
+/data/soft/neo4j-community-2.3.3/bin/neo4j-shell -file /data/toniher/script >> syn.out 2>> syn.err
+
 for file in $DIR/*
 do
 	echo $file
-        /data/soft/neo4j-community-2.3.3/bin/neo4j-shell -file $file >> syn.out 2>> syn.err
+	echo "import-cypher -b 10000 -d\"\t\" -i $file -o /data/toniher/out MATCH (n:MOL {id:{id}}) set n.synonyms = {synonyms}" > /data/toniher/script
+        parallel -j6 /data/soft/neo4j-community-2.3.3/bin/neo4j-shell -file /data/toniher/script >> syn.out 2>> syn.err
 done
