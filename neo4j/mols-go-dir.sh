8b1fc7513ebd18bbce61c7ec4b5a689580557db8
Toni Hermoso Pulido <toniher@cau.cat>
2016-04-14 23:41:48 +0200

    update taxon and goa

--- neo4j/mols-go-dir.sh
+++ neo4j/mols-go-dir.sh
@@ -1,10 +1,29 @@
 #DIR of parts
-DIR=$1
+DIR=/data/db/go/goa/molgoa
+
+mkdir -p $DIR; cd $DIR; split -l 10000000 ../gp_association.goa_uniprot.reduced gp_association.goa_uniprot.reduced
+
+echo "Modify files"
+
+for file in $DIR/*
+do
+		echo -e "id\tqualifier\tgoacc\tref\tevidence" |cat - $file > /data/toniher/tempfile && mv /data/toniher/tempfile $file
+
+done
+
+echo "CREATE INDEX ON :has_go(evidence);" > /data/toniher/script
+/data/soft/neo4j-community-2.3.3/bin/neo4j-shell -file /data/toniher/script >> syn.out 2>> syn.err
+echo "CREATE INDEX ON :has_go(ref);" > /data/toniher/script
+/data/soft/neo4j-community-2.3.3/bin/neo4j-shell -file /data/toniher/script >> syn.out 2>> syn.err
+echo "CREATE INDEX ON :has_go(qualifier);" > /data/toniher/script
+/data/soft/neo4j-community-2.3.3/bin/neo4j-shell -file /data/toniher/script >> syn.out 2>> syn.err
+
+
+echo "Neo4j importing"
 
 for file in $DIR/*
 do
 	echo $file
-	echo -e "id\tqualifier\tgoacc\tref\tevidence" |cat - $file > /data/toniher/tempfile && mv /data/toniher/tempfile $file
 	echo "import-cypher -b 10000 -d\"\t\" -i $file -o /data/toniher/out MATCH (c:MOL {id:{id}}), (p:GO_TERM {acc:{goacc}}) CREATE (c)-[:has_go { evidence: {evidence}, ref: {ref}, qualifier: {qualifier} }]->(p)" > /data/toniher/script
         /data/soft/neo4j-community-2.3.3/bin/neo4j-shell -file /data/toniher/script >> syn.out 2>> syn.err
 done
--- neo4j/mols-taxon-dir.sh
+++ neo4j/mols-taxon-dir.sh
@@ -1,10 +1,19 @@
 #DIR of parts
-DIR=$1
+DIR=/data/db/go/goa/moltaxon
+
+mkdir -p $DIR; cd $DIR; split -l 10000000 ../gp_information.goa_uniprot.reduced gp_information.goa_uniprot.reduced
+
+
+echo "Modify files"
+
+for file in $DIR/*
+do
+	echo -e "id\ttaxon:int" |cat - $file > /data/toniher/tempfile && mv /data/toniher/tempfile $file
+done
 
 for file in $DIR/*
 do
 	echo $file
-	echo -e "id\ttaxon:int" |cat - $file > /data/toniher/tempfile && mv /data/toniher/tempfile $file
 	echo "import-cypher -b 10000 -d\"\t\" -i $file -o /data/toniher/out MATCH (c:MOL {id:{id}}), (p:TAXID {id:{taxon}}) CREATE (c)-[:has_taxon]->(p)" > /data/toniher/script
         /data/soft/neo4j-community-2.3.3/bin/neo4j-shell -file /data/toniher/script >> syn.out 2>> syn.err
 done
