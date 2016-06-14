37a37e491c14a8bf551e773c8832da209ab947ae
toniher <toniher@cau.cat>
2016-06-14 13:02:29 +0200

    cleaning a bit

--- neo4j/README.md
+++ neo4j/README.md
@@ -1,13 +1,11 @@
 Scripts for importing NCBI Taxonomy and GeneOntology data to Neo4j
 
-Versions for Py2neo 1.6 and Py2neo 2.x
-
-* neo4j-go.py, neo4j2-go.py
+* neo4j2-go.py
     * http://www.geneontology.org/GO.downloads.database.shtml (mysql dump files)
 	* Argument files: term.txt, term_definition.txt, term2term.txt
-* neo4j-ncbi.py, neo4j2-ncbi.py
+* neo4j2-ncbi.py
 	* ftp://ftp.ncbi.nih.gov/pub/taxonomy/
 	* Argument files: nodes.dmp, names.dmp
 
-* cypher-query.shell (queries to speed up relationships addition, to be used with: https://github.com/jexp/neo4j-shell-tools )
+* uniprot.sh (queries to speed up relationships addition for UniProt entries, to be used with: https://github.com/jexp/neo4j-shell-tools )
 
--- neo4j/uniprot.sh
+++ neo4j/uniprot.sh
@@ -5,17 +5,17 @@ cut -f 1,2,3 gp_information.goa_uniprot.base | perl -F'\t' -lane ' if ($F[2]=~/^
 
 python neo4j2-synonyms-add-from-redis.py /data/db/go/goa/gp_information.goa_uniprot.protein > /data/db/go/goa/gp_information.goa_uniprot.extra
 
-nohup ./mols-dir.sh  &
+nohup ./mols-dir.sh
 
 
 # Adding relationships to Taxon
 cut -f 1,6 gp_information.goa_uniprot | perl -F'\t' -lane ' if ($F[0]!~/^\!/ && $F[1]=~/^taxon/ ) { my $id=$F[0]; my $tax=$F[1]; $tax=~s/taxon\://g; print $id, "\t", $tax; } ' > gp_information.goa_uniprot.reduced
 
-nohup ./mols-taxon-dir.sh  &
+nohup ./mols-taxon-dir.sh
 
 
 # Adding relationships to GO
 cut -f 1,2,3,4,5,6 gp_association.goa_uniprot | perl -F'\t' -lane ' if ($F[0]!~/^\!/ && $F[0]=~/^UniProt/ ) { print join("\t", @F[1..5]); } '  > gp_association.goa_uniprot.reduced
 
-nohup ./mols-go-dir.sh  &
+nohup ./mols-go-dir.sh
 
