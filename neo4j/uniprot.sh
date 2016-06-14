62044a9b124bbe1c1b2d553feed87dbf25c212e9
toniher <toniher@cau.cat>
2016-06-14 16:28:49 +0200

    cleaning and config improved

--- neo4j/uniprot.sh
+++ neo4j/uniprot.sh
@@ -1,21 +1,140 @@
+# CONFIG parameters
+
+NEO4JSHELL=/data/soft/neo4j-community-2.3.3/bin/neo4j-shell
+GOADIR=/data/db/go/goa
+MAPPINGDIR=/data/db/go/mapping
+MOMENTDIR=/data/toniher
+SCRIPTPATH=`pwd`
+
+INFOFILE=goa_uniprot_all.gpi
+GOAFILE=goa_uniprot_all.gpa
+
+#IDmapping: ftp://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz
+#Info Uniprot: ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/goa_uniprot_all.gpi.gz
+#GOA: ftp://ftp.ebi.ac.uk/pub/databases/GO/goa/UNIPROT/goa_uniprot_all.gpa.gz
+
+# Let's uncompress all files
+cd $GOADIR
+gunzip *gz
+
 # Base entries
-cut -f 1,3,5,6 gp_information.goa_uniprot | perl -F'\t' -lane ' if ($F[0]!~/^\!/ ) { $F[1]=~s/\"/\\"/g; print join( "\t", @F[0..2] ); } ' > gp_information.goa_uniprot.base
+cut -f 1,3,5,6 $INFOFILE | perl -F'\t' -lane ' if ($F[0]!~/^\!/ ) { $F[1]=~s/\"/\\"/g; print join( "\t", @F[0..2] ); } ' > $INFOFILE.base
+# We skip interaction stuff for now
+cut -f 1,2,3 $INFOFILE.base | perl -F'\t' -lane ' if ($F[2]=~/^protein/ ) { print $_; } ' > $INFOFILE.protein
 
-cut -f 1,2,3 gp_information.goa_uniprot.base | perl -F'\t' -lane ' if ($F[2]=~/^protein/ ) { print $_; } ' > gp_information.goa_uniprot.protein
+rm $INFOFILE.base
 
-python neo4j2-synonyms-add-from-redis.py /data/db/go/goa/gp_information.goa_uniprot.protein > /data/db/go/goa/gp_information.goa_uniprot.extra
+# Creating synonyms in Redis -> TODO, this MUST change
 
-nohup ./mols-dir.sh
+cd $MAPPINGDIR
+gunzip *gz
+
+DIR=$MAPPINGDIR/tmp
+mkdir -p $MAPPINGDIR/tmp
+python $SCRIPTPATH/neo4j2-synonyms-split.py $MAPPINGDIR/idmapping.dat $MAPPINGDIR/tmp
+
+for file in $DIR/*
+do
+	python $SCRIPTPATH/neo4j2-synonyms-redis.py $file
+done
+
+python $SCRIPTPATH/neo4j2-synonyms-add-from-redis.py $GOADIR/$INFOFILE.protein > $GOADIR/$INFOFILE.extra
+
+# Cleaning
+rm -rf $MAPPINGDIR/tmp
+
+# Adding mols
+
+# DIR of parts
+DIR=$GOADIR/mol
+
+mkdir -p $DIR; cd $DIR; split -l 10000000 ../$INFOFILE.extra $INFOFILE.extra
+
+echo "Modify files"
+
+for file in $DIR/*
+do
+	echo -e "id\tname\ttype\tsynonyms:string_array" |cat - $file > $MOMENTDIR/tempfile && mv $MOMENTDIR/tempfile $file
+done
+
+echo "CREATE CONSTRAINT ON (n:MOL) ASSERT n.id IS UNIQUE;" > $MOMENTDIR/script
+$NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+echo "CREATE INDEX ON :MOL(synonyms);" > $MOMENTDIR/script
+$NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+echo "CREATE INDEX ON :MOL(name);" > $MOMENTDIR/script 
+$NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+echo "CREATE INDEX ON :MOL(type);" > $MOMENTDIR/script
+$NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+
+echo "Neo4j importing"
+
+for file in $DIR/*
+do
+	echo $file
+	echo "import-cypher -b 10000 -d\"\t\" -i $file -o $MOMENTDIR/out CREATE (n:MOL { id:{id}, name:{name}, type:{type}, synonyms: { synonyms } })" > $MOMENTDIR/script
+       $NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+done
 
 
 # Adding relationships to Taxon
-cut -f 1,6 gp_information.goa_uniprot | perl -F'\t' -lane ' if ($F[0]!~/^\!/ && $F[1]=~/^taxon/ ) { my $id=$F[0]; my $tax=$F[1]; $tax=~s/taxon\://g; print $id, "\t", $tax; } ' > gp_information.goa_uniprot.reduced
+cut -f 1,6 $INFOFILE | perl -F'\t' -lane ' if ($F[0]!~/^\!/ && $F[1]=~/^taxon/ ) { my $id=$F[0]; my $tax=$F[1]; $tax=~s/taxon\://g; print $id, "\t", $tax; } ' > $INFOFILE.reduced
 
-nohup ./mols-taxon-dir.sh
+# DIR of parts
+DIR=$GOADIR/moltaxon
+
+mkdir -p $DIR; cd $DIR; split -l 10000000 ../$INFOFILE.reduced $INFOFILE.reduced
+
+
+echo "Modify files"
+
+for file in $DIR/*
+do
+	echo -e "id\ttaxon:int" |cat - $file > $MOMENTDIR/tempfile && mv $MOMENTDIR/tempfile $file
+done
+
+for file in $DIR/*
+do
+	echo $file
+	echo "import-cypher -b 10000 -d\"\t\" -i $file -o $MOMENTDIR/out MATCH (c:MOL {id:{id}}), (p:TAXID {id:{taxon}}) CREATE (c)-[:has_taxon]->(p)" > $MOMENTDIR/script
+       $NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+done
+
+rm $INFOFILE.reduced
 
 
 # Adding relationships to GO
-cut -f 1,2,3,4,5,6 gp_association.goa_uniprot | perl -F'\t' -lane ' if ($F[0]!~/^\!/ && $F[0]=~/^UniProt/ ) { print join("\t", @F[1..5]); } '  > gp_association.goa_uniprot.reduced
+cut -f 1,2,3,4,5,6 $GOAFILE | perl -F'\t' -lane ' if ($F[0]!~/^\!/ && $F[0]=~/^UniProt/ ) { print join("\t", @F[1..5]); } '  > $GOAFILE.reduced
 
-nohup ./mols-go-dir.sh
+#DIR of parts
+DIR=$GOADIR/molgoa
+
+mkdir -p $DIR; cd $DIR; split -l 10000000 ../$GOAFILE.reduced $GOAFILE.reduced
+
+echo "Modify files"
+
+for file in $DIR/*
+do
+		echo -e "id\tqualifier\tgoacc\tref\tevidence" |cat - $file > $MOMENTDIR/tempfile && mv $MOMENTDIR/tempfile $file
+
+done
+
+rm $INFOFILE.reduced
+
+
+echo "CREATE INDEX ON :has_go(evidence);" > $MOMENTDIR/script
+$NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+echo "CREATE INDEX ON :has_go(ref);" > $MOMENTDIR/script
+$NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+echo "CREATE INDEX ON :has_go(qualifier);" > $MOMENTDIR/script
+$NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+
+
+echo "Neo4j importing"
+
+for file in $DIR/*
+do
+	echo $file
+	echo "import-cypher -b 10000 -d\"\t\" -i $file -o $MOMENTDIR/out MATCH (c:MOL {id:{id}}), (p:GO_TERM {acc:{goacc}}) CREATE (c)-[:has_go { evidence: {evidence}, ref: {ref}, qualifier: {qualifier} }]->(p)" > $MOMENTDIR/script
+       $NEO4JSHELL -file $MOMENTDIR/script >> $MOMENTDIR/syn.out 2>> $MOMENTDIR/syn.err
+done
 
