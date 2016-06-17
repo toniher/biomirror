0e00729c55ea77eb1609733cdc76825728fc0a0b
toniher <toniher@cau.cat>
2016-06-17 12:26:22 +0200

    replace quotes

--- mysql/uniprot/idmapping.py
+++ mysql/uniprot/idmapping.py
@@ -6,6 +6,8 @@ import MySQLdb
 import json
 import gzip
 
+csv.field_size_limit(sys.maxsize)
+
 def main(argv):
         if len(sys.argv) < 2:
                 sys.exit()
@@ -64,7 +66,7 @@ def main(argv):
                         if ( row[0].startswith('!') ): #Avoid row with !
                                 continue
 
-                        cursor.execute('INSERT INTO idmapping VALUES("'+row[0]+'", "'+row[1]+'", "'+row[2]+'")')
+                        cursor.execute('INSERT INTO idmapping VALUES("'+row[0]+'", "'+row[1]+'", "'+row[2].replace( "\"", "\\\"" )+'")')
                         i = i+1
                         if (i == limit):
                                 i=0
