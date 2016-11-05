ec769b0fcf6c2637a4b53df62cb8a2e28a55ebef
Toni Hermoso Pulido <toniher@cau.cat>
2016-11-05 23:29:34 +0100

    rm part

--- mysql/ncbi/run-biosql.sh
+++ mysql/ncbi/run-biosql.sh
@@ -16,15 +16,10 @@ wget -c -t0 ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2pubmed.gz -o /dev/null
 wget -c -t0 ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2refseq.gz -o /dev/null
 wget -c -t0 ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_group.gz -o /dev/null
 wget -c -t0 ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_refseq_uniprotkb_collab.gz -o /dev/null
-wget -c -t0 ftp://ftp.pir.georgetown.edu/databases/idmapping/idmapping.tb.gz -o /dev/null
-wget -c -t0 http://archive.geneontology.org/latest-full/go_$month-assocdb-data.gz -o /dev/null
-gunzip go_$month-assocdb-data.gz
-gunzip gene_info.gz
-gunzip gene2accession.gz
-gunzip idmapping.tb.gz
+gunzip *.gz
+
 
 mysql -s -u$user -p$passwd -h$server $db < biosql.sql
-mysql -s -u$user -p$passwd -h$server $db < go_$month-assocdb-data
 mysql -s -u$user -p$passwd -h$server $db -e "LOAD DATA LOCAL INFILE '$path/gene2accession' INTO TABLE gene2accession FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES;"
 mysql -s -u$user -p$passwd -h$server $db -e "LOAD DATA LOCAL INFILE '$path/gene_info' INTO TABLE gene_info FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES;"
 mysql -s -u$user -p$passwd -h$server $db -e "LOAD DATA LOCAL INFILE '$path/gene2go' INTO TABLE gene2go FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES;"
