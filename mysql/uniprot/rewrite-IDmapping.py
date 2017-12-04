ccb3957881c65fbeef1db874d26e68ff7d101958
Toni Hermoso Pulido <toniher@cau.cat>
2017-12-04 15:20:50 +0100

    Upgrade stuff

--- mysql/uniprot/rewrite-IDmapping.py
+++ mysql/uniprot/rewrite-IDmapping.py
@@ -5,31 +5,33 @@ import pprint
 import sys
 
 def main(argv):
-		if len(sys.argv) < 2:
-				sys.exit()
+                if len(sys.argv) < 2:
+                                sys.exit()
+                
+                pre = ""
+                chunk = 1000
+                string = ""
+                count = 0
 
-		blackIds = dict()
-		pre = ""
-
-
-		if len( sys.argv ) > 2:
-
-			with open(sys.argv[2],'r') as f:
-					reader=csv.reader(f,delimiter='\t')
-					for row in reader:
-						blackIds[row[0]] = 1
-
-		with open(sys.argv[1],'r') as f:
-				reader=csv.reader(f,delimiter='\t')
-				for row in reader:
-
-                                        if row[0] not in blackIds :
-                                        
-                                                if row[0] != pre :
-                                                        pre = row[0]
-                                                        print row[0]+"\t"+"UniProtKB-AC"+"\t"+row[0]
-                                                print "\t".join( row )
-                                                
+                if len( sys.argv ) > 1:
+                
+                        with open(sys.argv[1],'r') as f:
+                                        reader=csv.reader(f,delimiter='\t')
+                                        for row in reader:
+                        
+                                                        
+                                                        if row[0] != pre :
+                                                                pre = row[0]
+                                                                string = string + row[0]+"\t"+"UniProtKB-AC"+"\t"+row[0]+"\n"
+                                                                string = string + "\t".join( row )+"\n"
+                                                        else :
+                                                                string = string + "\t".join( row )+"\n"                                        
+                                                        count = count + 1
+                                                        if count >= chunk :
+                                                                count = 0
+                                                                print string # Print if chunk size higger
+                                                                string = ""
+                        print string
 
 if __name__ == "__main__":
         main(sys.argv[1:])
--- mysql/uniprot/run-idmapping.sh
+++ mysql/uniprot/run-idmapping.sh
@@ -1,22 +1,37 @@
 #!/bin/sh
+set -ueo pipefail
 
 while read line; do
     declare "$line"
 done < "../config.sh"
 
+FILEDIR=files
+SCRIPTDIR=`pwd`
 
-mkdir -p files
+mkdir -p $FILEDIR
 
-cd files
+cd $FILEDIR
 rm -f *gz
 wget -c -t0 ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz -o /dev/null
 gunzip idmapping.dat.gz
 
 cd ../
 
-python rewrite-IDmapping.py files/idmapping.dat > files/idmapping.new.dat
+python rewrite-IDmapping.py $FILEDIR/idmapping.dat > $FILEDIR/idmapping.new.dat
+sed -i '/^$/d' $FILEDIR/idmapping.new.dat
 
-mysql -s -u$user -p$passwd -h$server $db < idmapping.sql
+rm $FILEDIR/idmapping.dat
 
-mysql -s -u$user -p$passwd -h$server $db -e "LOAD DATA LOCAL INFILE 'files/idmapping.new.dat' INTO TABLE idmapping FIELDS TERMINATED BY '\t' ENCLOSED BY '' "
+DIR=$FILEDIR/parts
+
+mkdir -p $DIR; cd $DIR; split --bytes=1G ../idmapping.new.dat idmapping.new.dat
+
+cd ..
+
+mysql -s -u$user -p$passwd -h$server $db < $SCRIPTDIR/idmapping.sql
+
+for file in $DIR/*
+do
+    mysql -s -u$user -p$passwd -h$server $db -e "SET @@session.unique_checks = 0; SET @@session.foreign_key_checks = 0; LOAD DATA LOCAL INFILE '${file}' INTO TABLE idmapping FIELDS TERMINATED BY '\t' ENCLOSED BY '' "
+done
 
