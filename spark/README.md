b7d61221ae19ac27836e5d2f92dc40c88d3e4e79
Toni Hermoso Pulido <toniher@cau.cat>
2020-08-28 17:29:15 +0200

    update image

--- spark/README.md
+++ spark/README.md
@@ -1,4 +1,4 @@
-Idmapping data can be downloaded from: 
+Idmapping data can be downloaded from:
 
     ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/idmapping.dat.gz
 
@@ -6,7 +6,7 @@ Stuff related to Hadoop, Spark, HBASE, etc.
 
     nohup python rewrite-IDmapping.py idmapping.dat > idmapping.new.dat &
     nohup hdfs dfs -copyFromLocal idmapping.new.dat idmapping.new.dat &> log &
-    nohup spark-submit cleanIdmapping.py  &> log & 
+    nohup spark-submit cleanIdmapping.py  &> log &
     #hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.columns=map:uniprot,map:db,HBASE_ROW_KEY idmapping /user/hbase/idmappingall
 
 retrieve:
@@ -17,5 +17,5 @@ retrieve:
 
 Put Dockerfile and simplify with Spark
 
-    docker run --network docker-spark_default --name my-spark-app -e ENABLE_INIT_DAEMON=false --link spark-master:spark-master -ti bde2020/spark-master:3.0.0-hadoop3.2 /bin/bash
-
+    docker build -t cleanidmapping .
+    docker run --volume /scratch/tmp:/scratch --network docker-spark_default --name cleanidmapping -e ENABLE_INIT_DAEMON=false --link spark-master:spark-master -ti cleanidmapping /bin/bash
--- spark/cleanIdmapping.py
+++ spark/cleanIdmapping.py
@@ -4,20 +4,18 @@
 import pyspark
 import argparse
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, length, size
-
+from pyspark.sql.functions import col, length
+from pyspark.sql.types import StructType, StructField
+from pyspark.sql.types import StringType
 import pprint
 
 spark = SparkSession.builder.master("local[1]") \
                     .appName('cleanIdmapping') \
                     .getOrCreate()
 
-from pyspark.sql.types import StructType, StructField
-from pyspark.sql.types import DoubleType, IntegerType, StringType
-
 parser = argparse.ArgumentParser(description="""Script cleaning Idmapping file""")
-parser.add_argument("-input",help="""Input file""")
-parser.add_argument("-output",help="""Output file""")
+parser.add_argument("-input", help="""Input file""")
+parser.add_argument("-output", help="""Output file""")
 args = parser.parse_args()
 
 schema = StructType([
@@ -26,12 +24,13 @@ schema = StructType([
     StructField("extern", StringType())
 ])
 
-df = spark.read.csv(args.input)
-    .schema(schema)
-    .option("header", "false")
-    .option("delimiter", "\t")
-    .option("mode", "DROPMALFORMED")
-    .dropDuplicates(['extern'])
-    .filter( length(col("extern")) > 4) )
+df = spark.read.format("csv") \
+     .option("header", False) \
+     .option("delimiter", "\t") \
+     .option("mode", "DROPMALFORMED") \
+     .schema(schema) \
+     .load(args.input) \
+     .dropDuplicates(['extern']) \
+     .filter(length(col("extern")) > 4)
 
 df.write.csv(args.output).options(delimiter="\t")
