d8cde59cca5a0918be0f4882d01e9fea5e81d2ec
Toni Hermoso Pulido <toniher@cau.cat>
2020-09-14 14:17:23 +0200

    update PDB

--- files/pdb/update_pdb.sh
+++ files/pdb/update_pdb.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
 # Update PDB and also formatted derived data
+
+OUTPUT=/nfs/db/pdb
+BLASTPATH=/software/bi/el7.2/version/ncbi-blast+/ncbi-blast-2.10.1+/bin
+BLASTVER=4
+
+DATE=`date +%Y-%m-%d`
+
+cd $OUTPUT/data/structures/divided
+rsync -rlpt -v -z --delete --port=33444 rsync.rcsb.org::ftp_data/structures/divided/pdb ./pdb
+
+cd $OUTPUT/derived_data
+rsync -rlpt -v -z --delete --port=33444 rsync.rcsb.org::ftp_derived/ .
+
+mkdir -p $OUTPUT/derived_data_format/blast/$DATE
+cp $OUTPUT/derived_data/pdb_seqres.txt $OUTPUT/derived_data_format/blast/$DATE
+cd $OUTPUT/derived_data_format/blast/$DATE
+
+${BLASTPATH}/makeblastdb -dbtype prot -parse_seqids -blastdb_version $BLASTVER -in pdb_seqres.fa
+
+# cd $OUTPUT/derived_data_format/blast
+# ln -s $DATE latest
