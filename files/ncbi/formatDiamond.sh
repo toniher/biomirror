b775e443fe728fff7872d39582e6c9eecd3341db
Toni Hermoso Pulido <toniher@cau.cat>
2020-10-06 18:14:17 +0200

    update diamond

--- files/ncbi/formatDiamond.sh
+++ files/ncbi/formatDiamond.sh
@@ -12,8 +12,8 @@ DIAMOND=singularity exec -e /software/bi/singularity/diamond/diamond-0.9.30.sif
 
 LISTDB=(cdd_delta env_nr landmark nr pdbaa refseq_protein swissprot tsa_nr)
 
-if [ ! -d ${BASEDIR}/${DATE}/blastdb/db ]
-  exit
+if [ ! -d ${BASEDIR}/${DATE}/blastdb/db ]; then
+  exit 1
 fi
 
 LOCATION=${BASEDIR}/${DATE}/blastdb/db
@@ -24,20 +24,18 @@ for i in ${LISTDB[@]}; do
 
   COUNT=$(find . -name "*${i}*" | wc -l)
 
-  if [ "$COUNT" -gt "0" ]
-
+  if [ "$COUNT" -gt "0" ]; then
     ${NCBIBLAST} blastdbcmd -dbtype prot -db ${i} -entry all -out ${i}.fa
   fi
 
-  if [ -f "${i}.fa" ]
-
+  if [ -f "${i}.fa" ]; then
     ${DIAMOND} diamond makedb --in ${i}.fa --db ${i}
   fi
 
-  if [ -f "${i}.dmnd" ]
+  if [ -f "${i}.dmnd" ]; then
     rm ${i}.fa
   fi
 
-  echo "Done ${i}\n"
+  echo "Done ${i}"
 
 done
