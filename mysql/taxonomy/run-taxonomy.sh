c281ebb2c10054da1e156cdf27eaf9126a58b530
Toni Hermoso Pulido <toniher@cau.cat>
2021-05-15 16:36:46 +0200

    play with paths

--- mysql/taxonomy/run-taxonomy.sh
+++ mysql/taxonomy/run-taxonomy.sh
@@ -14,7 +14,9 @@ db=$(jq .mysql.db $JSONFILE)
 
 cp -rf $taxondir/* $workdir
 
-mysql -s -u$user -p$password -h$host $db < taxonomy.sql
+curdir="$(dirname "$(realpath "$0")")"
+
+mysql -s -u$user -p$password -h$host $db < $curdir/taxonomy.sql
 mysql -s -u$user -p$password -h$host $db -e "LOAD DATA LOCAL INFILE '$workdir/names.dmp' INTO TABLE ncbi_names FIELDS TERMINATED BY '\t|\t' LINES TERMINATED BY '\t|\n' (tax_id, name_txt, unique_name, name_class);"
 mysql -s -u$user -p$password -h$host $db -e "LOAD DATA LOCAL INFILE '$workdir/nodes.dmp' INTO TABLE ncbi_nodes FIELDS TERMINATED BY '\t|\t' LINES TERMINATED BY '\t|\n' (tax_id, parent_tax_id,rank,embl_code,division_id, inherited_div_flag,genetic_code_id,inherited_GC_flag, mitochondrial_genetic_code_id,inherited_MGC_flag, GenBank_hidden_flag,hidden_subtree_root_flag,comments);"
 
