9e1ffe7dcf3a9de8941ba24394e0833e95012289
Toni Hermoso Pulido <toniher@cau.cat>
2022-08-10 15:58:41 +0200

    fix

--- terraform/config.sh
+++ terraform/config.sh
@@ -1,6 +1,7 @@
 export TF_VAR_key_name=key-nf
 export TF_VAR_profile=default
-export TF_VAR_credentials=/home/myuser/.aws/credentials
+export TF_VAR_credentials='["/home/myuser/.aws/credentials"]'
+export TF_VAR_region=eu-central-1
 export TF_VAR_vpc_id=vpc-68c6c103
 export TF_VAR_subnets='["subnet-8a280df7", "subnet-c54d6588", "subnet-b85ab5d2"]'
 # DB stuff
