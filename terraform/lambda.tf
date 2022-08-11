5bc4ef91952d699d9b6aa1c25f1dd9fab24e1629
Toni Hermoso Pulido <toniher@cau.cat>
2022-08-11 17:00:01 +0200

    moving on lambdas

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
 // TOOD: To be finished
 
 resource "aws_iam_role" "lambda_biomirror_role" {
-  name = "Lambda-biomirror-${random_string.rand.result}"
+  name = "lambda-biomirror-${random_string.rand.result}"
 
   assume_role_policy = <<EOF
 {
@@ -25,30 +25,44 @@ resource "aws_iam_role_policy_attachment" "lambda_role_policy" {
   policy_arn = aws_iam_policy.stop_start_ec2_policy.arn
 }
 
+// TODO: attach this policies
 // AmazonRDSFullAccess
 // AmazonRDSDataFullAccess
 // AmazonVPCFullAccess
 // AWSLambdaVPCAccessExecutionRole
 
-data "archive_file" "ec2-lambda-zip" {
+// TODO: More love needed here. Layers: https://www.linkedin.com/pulse/como-deployar-aws-lambda-layers-com-terraform-e-nodejs-gasparoto/?trk=public_profile_article_view
+data "archive_file" "db-lambda-zip" {
   type        = "zip"
-  output_path = "ec2_lambda_handler.zip"
+  source_file   = "index.js"
+  output_path   = "lambda_function.zip"
   source {
-    content  = templatefile("ec2_lambda_handler.tpl", { region = var.region })
-    filename = "ec2_lambda_handler.py"
+    content  = "index.js"
+    filename = "index.js"
   }
 }
 
-resource "aws_lambda_function" "stop_ec2_lambda" {
-  filename      = data.archive_file.ec2-lambda-zip.output_path
-  function_name = "stopEC2Lambda-${random_string.rand.result}"
-  role          = aws_iam_role.stop_start_ec2_role.arn
-  handler       = "ec2_lambda_handler.stop"
+resource "aws_lambda_function" "create_rds_database" {
+  filename      = data.archive_file.db-lambda-zip.output_path
+  function_name = "create-db-${random_string.rand.result}"
+  role          = aws_iam_role.lambda_biomirror_role.arn
+  handler       = "index.handler"
 
-  source_code_hash = filebase64sha256(data.archive_file.ec2-lambda-zip.output_path)
+  source_code_hash = filebase64sha256(data.archive_file.db-lambda-zip.output_path)
 
-  runtime     = "python3.7"
-  memory_size = "250"
+  runtime     = "nodejs16.x"
   timeout     = "60"
+
+
+  environment {
+    variables = {  
+      RDS_HOSTNAME = aws_db_instance.mydb.address
+      RDS_USERNAME = root
+      RDS_PASSWORD = var.db_password
+      RDS_PORT = var.db_port
+    }
+  }
+
+  depends_on = [ aws_db_instance.mydb ]
 }
 
