5257b8e48526c3f434c9f0d5aaa4c1e7f5612a60
Toni Hermoso Pulido <toniher@cau.cat>
2022-08-21 15:20:16 +0200

    update job

--- terraform/glue_job.tf
+++ terraform/glue_job.tf
@@ -9,10 +9,14 @@ resource "aws_s3_object" "glue_script" {
 
 resource "aws_glue_job" "glue_job" {
     name     = "glue-job-${random_string.rand.result}"
+    description = "Glue Job for biomirror"
     role_arn = aws_iam_role.glue-rds-role.arn
     max_retries  = 1
     glue_version = "3.0"
     worker_type  = "G.1X"
+    number_of_workers = 10
+    
+    execution_class = "STANDARD" //Alternative is FLEX
 
     command {
         script_location = "s3://${var.bucket_scripts}/s3-to-rds.py"
@@ -20,6 +24,17 @@ resource "aws_glue_job" "glue_job" {
     }
 
     execution_property {
-        max_concurrent_runs = 5
+        max_concurrent_runs = 10
+    }
+
+    default_arguments = {
+        "--job-bookmark-option" = "job-bookmark-enable"
+        "--enable-metrics" = "true"
+        "--enable-continuous-cloudwatch-log" = "true"
+        "--enable-spark-ui" = "true"
+        "--enable-glue-datacatalog" = "true"
+        "--enable-job-insights" = "true"
+        "--TempDir" = "s3://${var.bucket_scripts}/tmp/"
+        "--spark-event-logs-path" = "s3://${var.bucket_scripts}/logs/"
     }
 }
