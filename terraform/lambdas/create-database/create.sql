1462e31c17a82ba5bcdaec55b555de285fc2003f
Toni Hermoso Pulido <toniher@cau.cat>
2022-09-06 16:57:43 +0200

    character set and collate

--- terraform/lambdas/create-database/create.sql
+++ terraform/lambdas/create-database/create.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS `biomirror`; 
 USE `biomirror`;
 DROP TABLE IF EXISTS `idmapping`;
-CREATE TABLE `idmapping` ( `uniprot` varchar(16) NOT NULL DEFAULT '', `db` varchar(24) NOT NULL DEFAULT '', `external` varchar(72) NOT NULL DEFAULT '' ) ENGINE=Aria CHARACTER SET latin1 COLLATE utf8mb4_bin;
+CREATE TABLE `idmapping` ( `uniprot` varchar(16) NOT NULL DEFAULT '', `db` varchar(24) NOT NULL DEFAULT '', `external` varchar(72) NOT NULL DEFAULT '' ) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin';
